90d8579d59c6bf10499905c0e79125c4323ffc0f
Martin Winter <martin.winter@student.tugraz.at>
2017-04-26 10:48:14 +0200

    [JW],[MW] Fixed testcases for list view according to new layout

--- app/src/androidTest/java/at/sw2017/awesomeinc/awesomeplayer/ListViewInstrumentedTest.java
+++ app/src/androidTest/java/at/sw2017/awesomeinc/awesomeplayer/ListViewInstrumentedTest.java
@@ -2,6 +2,12 @@ package at.sw2017.awesomeinc.awesomeplayer;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.action.GeneralClickAction;
+import android.support.test.espresso.action.GeneralLocation;
+import android.support.test.espresso.action.GeneralSwipeAction;
+import android.support.test.espresso.action.Press;
+import android.support.test.espresso.action.Swipe;
+import android.support.test.espresso.action.Tap;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.espresso.contrib.RecyclerViewActions;
@@ -12,10 +18,15 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.swipeUp;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.hasToString;
+import static org.hamcrest.core.StringStartsWith.startsWith;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
@@ -46,13 +57,30 @@ public class ListViewInstrumentedTest {
 
     @Test
     public void test_ListViewScroll() throws Exception {
-        onView(withId(R.id.lst_tracklist)).perform(ViewActions.scrollTo()).check(ViewAssertions.matches(isDisplayed()));
+        onView(withId(R.id.content_main)).perform(new GeneralSwipeAction(Swipe.FAST, GeneralLocation.CENTER_LEFT, GeneralLocation.CENTER, Press.FINGER));
+        onView(withText("Songs")).perform(click());
+        onView(withId(R.id.content_main)).perform(swipeUp());
+
+    }
+
+    @Test
+    public void test_MenuSongs() throws Exception {
+        onView(withId(R.id.content_main)).perform(new GeneralSwipeAction(Swipe.FAST, GeneralLocation.CENTER_LEFT, GeneralLocation.CENTER, Press.FINGER));
+        onView(withText("Songs")).perform(click());
+    }
+
+    @Test
+    public void test_MenuPlaylist() throws Exception {
+        onView(withId(R.id.content_main)).perform(new GeneralSwipeAction(Swipe.FAST, GeneralLocation.CENTER_LEFT, GeneralLocation.CENTER, Press.FINGER));
+        onView(withText("Playlists")).perform(click());
 
     }
 
     @Test
     public void test_ListViewSongNameClick() throws Exception {
-        onView(withId(R.id.lst_tracklist)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.content_main)).perform(new GeneralSwipeAction(Swipe.FAST, GeneralLocation.CENTER_LEFT, GeneralLocation.CENTER, Press.FINGER));
+        onView(withText("Songs")).perform(click());
+        onView(withId(R.id.content_main)).perform(new GeneralClickAction(Tap.SINGLE, GeneralLocation.CENTER_LEFT, Press.FINGER));
 
     }
 
