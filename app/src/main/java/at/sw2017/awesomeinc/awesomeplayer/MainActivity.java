f147540a6e1598ad0c991f3c03cbea52a5f16cca
Martin Winter <martin.winter@student.tugraz.at>
2017-05-17 10:23:04 +0200

    [MS],[MW] Added search bar and first TCs

--- app/src/main/java/at/sw2017/awesomeinc/awesomeplayer/MainActivity.java
+++ app/src/main/java/at/sw2017/awesomeinc/awesomeplayer/MainActivity.java
@@ -11,12 +11,14 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -63,6 +65,10 @@ public class MainActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        final MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setQueryHint("Search for awesome music...");
+        //searchView.setOnQueryTextListener(this);
         return true;
     }
 
